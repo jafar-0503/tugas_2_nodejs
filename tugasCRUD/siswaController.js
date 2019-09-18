b7a2a8f0f6a5ebf92a0698b1ccc5c1bf97b94d75
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-09-18 22:24:16 +0700

    tugas_2_nodejs
    ok

---
+++ tugasCRUD/siswaController.js
@@ -0,0 +1,84 @@
+// siswaController.js
+// Import siswa model
+Siswa = require('./siswaModel');
+// Handle index actions
+exports.index = function (req, res) {
+   Siswa.get(function (err, siswas) {
+       if (err) {
+           res.json({
+               status: "error",
+               message: err,
+           });
+       }
+       res.json({
+           status: "success",
+           message: "Siswa retrieved successfully",
+           data: siswas
+       });
+   });
+};
+// Handle create siswa actions
+exports.new = function (req, res) {
+   var siswa = new Siswa();
+   siswa.name = req.body.name ? req.body.name : siswa.name;
+   siswa.birth = req.body.birth;
+   siswa.gender = req.body.gender;
+   siswa.hobby = req.body.hobby;
+
+// save the siswa and check for errors
+   siswa.save(function (err) {
+       // if (err)
+       //     res.json(err);
+res.json({
+           message: 'New siswa created!',
+           data: siswa
+       });
+   });
+};
+
+// Handle view siswa info
+exports.view = function (req, res) {
+   Siswa.findById(req.params.siswa_id, function (err, siswa) {
+       if (err)
+           res.send(err);
+       res.json({
+           message: 'Siswa details loading..',
+           data: siswa
+       });
+   });
+};
+
+// Handle update siswa info
+exports.update = function (req, res) {
+Siswa.findById(req.params.siswa_id, function (err, siswa) {
+       if (err)
+           res.send(err);
+       siswa.name = req.body.name ? req.body.name : siswa.name;
+       siswa.birth = req.body.birth;
+       siswa.gender = req.body.gender;
+       siswa.hobby = req.body.hobby;
+
+// save the siswa and check for errors
+       siswa.save(function (err) {
+           if (err)
+               res.json(err);
+           res.json({
+               message: 'Siswa Info updated',
+               data: siswa
+           });
+       });
+   });
+};
+// Handle delete siswa
+exports.delete = function (req, res) {
+   Siswa.remove({
+       _id: req.params.siswa_id
+   }, function (err, siswa) {
+       if (err)
+           res.send(err);
+res.json({
+           status: "Success",
+           message: 'Siswa deleted'
+       });
+   });
+};
